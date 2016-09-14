81d06146559fa5af54aab2bd43f4eb8d9b90b091
Andy Matuschak <andy@khanacademy.org>
2016-09-14 16:20:33 -0700

    Factored out property repetition in EntryList

--- imports/ui/components/EntryList.jsx
+++ imports/ui/components/EntryList.jsx
@@ -16,35 +16,30 @@ export default (props) => {
     event.preventDefault();
   };
 
+  const propsForEntry = (entry) => {
+    return {
+      key: entry._id,
+      entry,
+      onChange: props.onChangeEntry,
+      onDelete: () => props.onDeleteEntry(entry._id),
+      onDropImage: (files, callback) => props.onDropImage(entry._id, files, callback),
+      onChangeRecommending: (isNewlyRecommending) => props.onChangeRecommending(entry._id, isNewlyRecommending),
+      onChangeViewing: (isNewlyViewing) => props.onChangeViewing(entry._id, isNewlyViewing),
+      onChangeURL: (newURL) => props.onChangeURL(entry._id, newURL),
+      onStartDiscussionThread: () => {props.onStartDiscussionThread(entry._id)},
+      disabled: props.disabled,
+    };
+  };
+
   const { focusedEntry } = props;
   return <div className="entryList">
     {props.entries.map((entry) => (
       <EntryCell
-        key={entry._id}
-        entry={entry}
-        onChange={props.onChangeEntry}
-        onDelete={() => props.onDeleteEntry(entry._id)}
-        onDropImage={(files, callback) => props.onDropImage(entry._id, files, callback) }
-        onChangeRecommending={(isNewlyRecommending) => props.onChangeRecommending(entry._id, isNewlyRecommending)}
-        onChangeViewing={(isNewlyViewing) => props.onChangeViewing(entry._id, isNewlyViewing)}
-        onChangeURL={(newURL) => props.onChangeURL(entry._id, newURL)}
-        onStartDiscussionThread={() => {props.onStartDiscussionThread(entry._id)}}
-        disabled={props.disabled}
+        {...propsForEntry(entry)}
       />
     ))}
     <Portal closeOnEsc closeOnOutsideClick isOpened={!!focusedEntry} className="lightbox" onClose={clearFocusedEntry}>
-      {focusedEntry ? <Entry
-        key={focusedEntry._id}
-        entry={focusedEntry}
-        onChange={props.onChangeEntry}
-        onDelete={() => props.onDeleteEntry(focusedEntry._id)}
-        onDropImage={(files, callback) => props.onDropImage(focusedEntry._id, files, callback) }
-        onChangeRecommending={(isNewlyRecommending) => props.onChangeRecommending(focusedEntry._id, isNewlyRecommending)}
-        onChangeViewing={(isNewlyViewing) => props.onChangeViewing(focusedEntry._id, isNewlyViewing)}
-        onChangeURL={(newURL) => props.onChangeURL(focusedEntry._id, newURL)}
-        onStartDiscussionThread={() => {props.onStartDiscussionThread(focusedEntry._id)}}
-        disabled={props.disabled}
-      /> : "" }
+      {focusedEntry ? <Entry {...propsForEntry(focusedEntry)} /> : "" }
     </Portal>
   </div>
 };
