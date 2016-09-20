afc3e26cb42168341c20fd75d682adf1745edc32
Andy Matuschak <andy@khanacademy.org>
2016-09-19 17:38:52 -0700

    Fix bug where clicking editor toolbar would close lightbox
    This is a pretty hacky solution. Ah, well; it'll do for now.
    
    Closes https://app.asana.com/0/inbox/15045191754661/183532972943812/183532975547428.

--- imports/ui/components/EntryList.jsx
+++ imports/ui/components/EntryList.jsx
@@ -6,46 +6,57 @@ import URLSearchParams from 'url-search-params';
 import Entry from './Entry.jsx';
 import EntryCell from './EntryCell.jsx';
 
-export default (props) => {
-  const clearFocusedEntry = (event) => {
-    let newURL = new URL(document.location);
-    const params = new URLSearchParams(newURL.search.slice(1));
-    params.delete("entry");
-    newURL.search = params.toString();
-    browserHistory.replace(newURL.toString());
-    
-    if (event) {
-      event.preventDefault();
-    }
-  };
+export default class EntryList extends React.Component {
+  render() {
+    const props = this.props;
 
-  const propsForEntry = (entry) => {
-    return {
-      key: entry._id,
-      entry,
-      onChange: props.onChangeEntry,
-      onDelete: () => {
-        clearFocusedEntry();
-        props.onDeleteEntry(entry._id);
-      },
-      onDropImage: (files, callback) => props.onDropImage(entry._id, files, callback),
-      onChangeRecommending: (isNewlyRecommending) => props.onChangeRecommending(entry._id, isNewlyRecommending),
-      onChangeViewing: (isNewlyViewing) => props.onChangeViewing(entry._id, isNewlyViewing),
-      onChangeURL: (newURL) => props.onChangeURL(entry._id, newURL),
-      onStartDiscussionThread: () => {props.onStartDiscussionThread(entry._id)},
-      disabled: props.disabled,
+    const clearFocusedEntry = (event) => {
+      let newURL = new URL(document.location);
+      const params = new URLSearchParams(newURL.search.slice(1));
+      params.delete("entry");
+      newURL.search = params.toString();
+      browserHistory.replace(newURL.toString());
+
+      if (event) {
+        event.preventDefault();
+      }
     };
-  };
 
-  const { focusedEntry } = props;
-  return <div className="entryList">
-    {props.entries.map((entry) => (
-      <EntryCell
-        {...propsForEntry(entry)}
-      />
-    ))}
-    <Portal closeOnEsc closeOnOutsideClick isOpened={!!focusedEntry} className="lightbox" onClose={clearFocusedEntry}>
-      {focusedEntry ? <Entry {...propsForEntry(focusedEntry)} /> : "" }
-    </Portal>
-  </div>
+    const propsForEntry = (entry) => {
+      return {
+        key: entry._id,
+        entry,
+        onChange: props.onChangeEntry,
+        onDelete: () => {
+          clearFocusedEntry();
+          props.onDeleteEntry(entry._id);
+        },
+        onDropImage: (files, callback) => props.onDropImage(entry._id, files, callback),
+        onChangeRecommending: (isNewlyRecommending) => props.onChangeRecommending(entry._id, isNewlyRecommending),
+        onChangeViewing: (isNewlyViewing) => props.onChangeViewing(entry._id, isNewlyViewing),
+        onChangeURL: (newURL) => props.onChangeURL(entry._id, newURL),
+        onStartDiscussionThread: () => {props.onStartDiscussionThread(entry._id)},
+        disabled: props.disabled,
+      };
+    };
+
+    const { focusedEntry } = props;
+    return <div className="entryList">
+      {props.entries.map((entry) => (
+        <EntryCell
+          {...propsForEntry(entry)}
+        />
+      ))}
+      <Portal closeOnEsc isOpened={!!focusedEntry} className="lightbox" onClose={clearFocusedEntry} ref="lightbox">
+        <div onClick={(event) => {
+            if (event.target === this.refs.lightboxBackground) {
+              this.refs.lightbox.closePortal();
+              event.stopPropagation();
+            }
+        }} ref="lightboxBackground">
+          {focusedEntry ? <Entry {...propsForEntry(focusedEntry)} /> : "" }
+        </div>
+      </Portal>
+    </div>;
+  }
 };
